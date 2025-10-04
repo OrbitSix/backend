b67e25fef56b5c17c426f9b7a3413479766f83ff
Hasan Md Arik <hasanmdarik@gmail.com>
2025-10-04 21:41:47 +0600

    Add rabbitmq handler

---
+++ rabbitmq.py
@@ -0,0 +1,33 @@
+import pika
+import os
+
+class RabbitMQ:
+    def __init__(self):
+        self.user = os.getenv('RABBITMQ_USER', 'user')
+        self.password = os.getenv('RABBITMQ_PASSWORD', 'password')
+        self.host = "127.0.0.1"
+        self.port = 5672
+        self.connection = None
+        self.channel = None
+        self.connect()
+
+    def connect(self):
+        credentials = pika.PlainCredentials(self.user, self.password)
+        parameters = pika.ConnectionParameters(host=self.host, port=self.port, credentials=credentials, virtual_host="orbitsix-mq")
+        self.connection = pika.BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+
+    def close(self):
+        if self.connection and not self.connection.is_closed:
+            self.connection.close()
+
+    def publish(self, message):
+        if not self.channel:
+            raise Exception("Connection is not established.")
+        self.channel.basic_publish(exchange="main-exchange",
+                                   routing_key="reasoning",
+                                   body=message,
+                                   properties=pika.BasicProperties(
+                                       delivery_mode=2,  
+                                   ))
+        
