8ec6d446bc70ef245f571f6df23a0b2facb081e2
Hasan Md Arik <hasanmdarik@gmail.com>
2025-10-04 21:43:14 +0600

    Manual predict done with placeholder value

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from uuid import uuid4
+from rabbitmq import RabbitMQ
+import pika
+import json
 
 class ManualPredictionReq(BaseModel):
     # Planetary and Orbital Params
@@ -20,22 +23,40 @@ class ManualPredictionReq(BaseModel):
     # Data Quality and Confidence
     koi_model_snr: float
 
-
+rabbitmq = RabbitMQ()
+mq_channel = rabbitmq.channel
 app = FastAPI()
 
 @app.post("/manual-predict") 
 async def manualPrediction(req_body: ManualPredictionReq):
     # Parse the request body
     print(req_body.model_dump())
+
     # Generate prediction
-    
+    score  = 0.93
+    res = "Not Exoplanet"
+    if score >= 0.65:
+        res= "Exoplanet"
+
     # Generate UUID
-    req_id = uuid4()
-    print("Req Id:", req_id.__str__())
+    req_id = uuid4().__str__()
+    
     # Pass reasoning job to MQ
+    mq_channel.basic_publish(exchange="main-exchange",
+                            routing_key="reasoning",
+                            body=json.dumps({
+                                    "id": req_id,
+                                    **req_body.model_dump()
+                                    }),
+                            properties=pika.BasicProperties(delivery_mode=2)
+                            )
 
     # Return response
     return {
-        "verdict": "Exoplanet",
-        "score": 0.93,
+        "id": req_id,
+        "verdict": res,
+        "score": score,
     }
+
+
+    
