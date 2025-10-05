63dcf8f04bcfd3e7d8208bc5ea24a837d1c3b6f1
Hasan Md Arik <hasanmdarik@gmail.com>
2025-10-05 19:33:45 +0600

    Add deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,9 @@ git clone https://HasanMdArik:$GITHUB_KEY@github.com/orbitsix/backend.git
 cd ./backend/
 python3 -m venv venv
 source venv/bin/activate
+pip3 install -r requirements.txt
+touch .env
+cp service/backend.service /etc/systemd/system
+systemctl daemon-reload
+systemctl start backend.service
+systemctl enable backend.service
